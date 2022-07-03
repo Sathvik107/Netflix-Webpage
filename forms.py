faf59fcdd22f73377d2be28972d591692275c7eb
Sathvik107 <103804079+Sathvik107@users.noreply.github.com>
2022-07-03 22:46:41 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms.widgets import DateInput
-from .models import users
-class usersform(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = users
-        fields = "__all__"
